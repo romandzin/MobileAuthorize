91559cbcb14d8619bbef7ff1cb9fe77ff0ca7966
romandzin <redrenjer@yandex.ru>
2023-03-03 12:13:50 +0300

    Complete 2.0

--- app/src/main/java/com/example/mobileauthorize/MainScreenActivity.kt
+++ app/src/main/java/com/example/mobileauthorize/MainScreenActivity.kt
@@ -1,12 +1,16 @@
 package com.example.mobileauthorize
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.mobileauthorize.databinding.ActivityMainPageBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlin.math.log
 
 
 class MainScreenActivity : AppCompatActivity() {
@@ -22,20 +26,25 @@ class MainScreenActivity : AppCompatActivity() {
         val navigation = binding.bottomNav
         mainPageFragment = MainPageFragment()
         profileFragment = ProfileFragment()
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                onBackPressed()
+            }
+        })
         navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
-        loadFragment(mainPageFragment)
+        loadFragment(mainPageFragment, "main")
     }
 
     private val onNavigationItemSelectedListener: BottomNavigationView.OnNavigationItemSelectedListener =
         object : BottomNavigationView.OnNavigationItemSelectedListener {
             override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                when (item.getItemId()) {
+                when (item.itemId) {
                     R.id.home -> {
-                        loadFragment(mainPageFragment)
+                        loadFragment(mainPageFragment, "main")
                         return true
                     }
                     R.id.profile -> {
-                        loadFragment(profileFragment)
+                        loadFragment(profileFragment, "profile")
                         return true
                     }
                 }
@@ -43,11 +52,19 @@ class MainScreenActivity : AppCompatActivity() {
             }
         }
 
-    private fun loadFragment(fragment: Fragment) {
+    private fun loadFragment(fragment: Fragment, tag: String) {
         val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.fl_content, fragment, "main")
-        ft.addToBackStack("main")
+        ft.replace(R.id.fl_content, fragment, tag)
+        ft.addToBackStack(tag)
         ft.commit()
+    }
+    override fun onBackPressed() {
+        val fragment = supportFragmentManager.fragments.last()
+        if (fragment is MainPageFragment) {
+
+        }
+        else supportFragmentManager.popBackStack()
+
     }
 
 
